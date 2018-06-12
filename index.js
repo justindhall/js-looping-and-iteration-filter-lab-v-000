6785cd99f52633a324e9522b7c2a287f85fa113a
Justin Hall <hall.justin.d@gmail.com>
2018-06-12 18:36:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 function findMatching(drivers, name){
   
-  return drivers.filter(function (driverName) { return driverName.toUpcase() === name.toUpcase();});
+  return drivers.filter(function (driverName) { return driverName.toUppercase() === name.toUppercase();});
   
 }
